7485c4d4bd98cc8c77dd3827f07140eb49823e30
Horváth Balázs <balazsjdp@gmail.com>
2026-01-31 12:14:31 +0100

    Relocate useapi hook

--- app/api/client/categories.ts
+++ app/api/client/categories.ts
@@ -1,5 +1,5 @@
 import { Category } from "@/types/category";
-import { useApi } from "@/hooks/use-api";
+import { useApi } from "@/app/api/use-api";
 
 export const useCategories = () => {
   return useApi<Category[]>("/categories");
--- app/api/client/config.ts
+++ app/api/client/config.ts
@@ -1,4 +1,4 @@
-import { useApi } from "@/hooks/use-api";
+import { useApi } from "@/app/api/use-api";
 import { Config } from "@/store/config/config-store";
 
 export const useConfig = () => {
